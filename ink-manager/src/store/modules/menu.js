c7f8ac5caa754c418edcbe4e37f7bcf82bb193cc
zxh <13605560342@163.com>
2021-01-16 22:23:35 +0800

    开始重构vuex

---
+++ ink-manager/src/store/modules/menu.js
@@ -0,0 +1,17 @@
+export const menu = {
+    state: () => ({ 
+        menuRelativeUrl:''
+     }),
+     getters: { 
+        getMenuRelativeUrl(state){
+            return state.menuRelativeUrl
+        }
+      },
+    mutations: { 
+        
+    },
+    actions: { 
+        
+     },
+
+}
